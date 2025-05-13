4403129e1e36f13bb98fd93a1ad8595524e9f557
Stephen Abello <stephen.abello@combodo.com>
2025-05-13 10:51:59 +0200

    N°7793 - First version for the extension aiming iTop 3.2. Modified some urls in SCSS / Twig files and added global twig variable as Controller to access them

---
+++ src/Router/GlobalVariablesControllerRouter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Combodo\iTop\Portal;
+
+use Combodo\iTop\Portal\Routing\ItopExtensionsExtraRoutes;
+
+ItopExtensionsExtraRoutes::AddControllersClasses(
+	array(
+	'Combodo\iTop\Portal\Twig\PreferencesGlobal'
+	)
+);
+
+ItopExtensionsExtraRoutes::AddControllersClasses(
+	array(
+	'Combodo\iTop\Portal\Twig\AppIconUrlGlobal'
+	)
+);
