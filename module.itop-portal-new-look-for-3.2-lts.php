82ee4f678ab7f4d4df8d2354837dff82bd6f3b6f
Stephen Abello <stephen.abello@combodo.com>
2026-03-05 15:02:21 +0100

    Update module version

--- module.itop-portal-new-look-for-3.2-lts.php
+++ module.itop-portal-new-look-for-3.2-lts.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (C) 2013-2025 Combodo SAS
  *
@@ -20,39 +21,39 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-portal-new-look-for-3.2-lts/1.0.1',
-	array(
-        // Identification
-        'label' => 'Portal new look for 3.2 LTS',
-        'category' => 'Portal',
-        // Setup
-        'dependencies' => array(
-            'itop-portal-base/3.2.1||itop-portal/3.2.1||itop-portal-business-partner/1.1.0',
-		),
+	'itop-portal-new-look-for-3.2-lts/1.0.2',
+	[
+		// Identification
+		'label' => 'Portal new look for 3.2 LTS',
+		'category' => 'Portal',
+		// Setup
+		'dependencies' => [
+			'itop-portal-base/3.2.1||itop-portal/3.2.1||itop-portal-business-partner/1.1.0',
+		],
 		'mandatory' => false,
 		'visible' => true,
 		// Components
-		'datamodel' => array(
+		'datamodel' => [
 			 '../itop-portal-base/portal/vendor/autoload.php',
 			'src/Twig/PreferencesGlobal.php',
 			'src/Twig/AppIconUrlGlobal.php',
 			'src/Router/GlobalVariablesControllerRouter.php',
 			'src/Router/PreferencesRouter.php',
-			'vendor/autoload.php'
-		),
-		'webservice' => array(
-		),
-		'dictionary' => array(
-		),
-		'data.struct' => array(
-		),
-		'data.sample' => array(
-		),
+			'vendor/autoload.php',
+		],
+		'webservice' => [
+		],
+		'dictionary' => [
+		],
+		'data.struct' => [
+		],
+		'data.sample' => [
+		],
 		// Documentation
 		'doc.manual_setup' => '',
 		'doc.more_information' => '',
 		// Default settings
-		'settings' => array(
-		),
-	)
+		'settings' => [
+		],
+	]
 );
